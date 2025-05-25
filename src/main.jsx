855544da8d1eabe9be11faa0e35de5de1aa139c4
saurav5085 <153294397+saurav5085@users.noreply.github.com>
2025-05-25 12:51:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./App";       // pulls in the default export from src/App.jsx
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
