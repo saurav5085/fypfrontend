24c5eec38cf4f32b655f8c2b3216d841ea54a88c
saurav5085 <153294397+saurav5085@users.noreply.github.com>
2025-05-25 12:18:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,19 +17,18 @@
 // }
 
 // export default App;
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
 export default App;
+
